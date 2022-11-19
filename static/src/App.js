b29a7bbc3b546a33c4343dc4da1be8f0b7a019bd
Stuti Agrawal <stutia3@illinois.edu>
2022-11-19 00:50:26 -0800

    created a basic ui

--- static/src/App.js
+++ static/src/App.js
@@ -1,17 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+// import Slider from '@mui/material/Slider';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 import Slider from '@mui/material/Slider';
-
-
-
-
+import Button from '@mui/material/Button';
+import React, { useState } from 'react';
 
 function App() {
-  
+  const [showResult, setShowResult] = useState(false);
+
   return (
     <div className="App">
       <h1> Mood-based Spotify song recommendations </h1>
-      <header className="App-header">
+
+      <Container  maxWidth="sm">
+        <Grid container columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        {showResult == false &&
+        <Grid item xs={6}>
+          <h2>What's the mood today?</h2>
+          mood 1
+          <Slider
+            size="small"
+            defaultValue={50}
+            aria-label="Small"
+            valueLabelDisplay="auto"
+          />
+
+          mood 2
+          <Slider
+            size="small"
+            defaultValue={50}
+            aria-label="Small"
+            valueLabelDisplay="auto"
+          />
+
+          mood 3
+          <Slider
+            size="small"
+            defaultValue={50}
+            aria-label="Small"
+            valueLabelDisplay="auto"
+          />
+
+          mood 4
+          <Slider
+            size="small"
+            defaultValue={50}
+            aria-label="Small"
+            valueLabelDisplay="auto"
+          />
+
+          mood 5
+          <Slider
+            size="small"
+            defaultValue={50}
+            aria-label="Small"
+            valueLabelDisplay="auto"
+          />
+          <Button variant="contained" color="success" onClick = {() => setShowResult(true)}>
+            Done
+          </Button>
+        </Grid>
+}
+        {showResult == true &&
+        <Grid item xs={6}>
+          <h2>Here is your song for the day</h2>
+          <Button variant="contained" color="success" onClick = {() => setShowResult(false)}>
+            Back
+          </Button>
+        </Grid>
+}
+      </Grid>
+      </Container>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           What's the mood today?
@@ -24,13 +86,13 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
       {/* <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
         <VolumeDown />
         <Slider aria-label="Volume" value={value} onChange={handleChange} />
         <VolumeUp />
       </Stack> */}
-      <Slider disabled defaultValue={30} aria-label="Disabled slider" />
+      {/* <Slider disabled defaultValue={30} aria-label="Disabled slider" /> */}
       {/* <Slider
         aria-label="Small steps"
         defaultValue={0.00000005}
