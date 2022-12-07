4b6c1b4cb5e104d3c37a3b5f96ce2b94ce6bc68a
Lisa Leung <ljleung2@illinois.edu>
2022-12-07 15:58:10 -0600

    Got song recs to work

--- app.py
+++ app.py
@@ -41,8 +41,6 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager) #spo
 
 # lets authorize and retrieve spotify object
 spotify = auth()
-moods = ["happy", "sad", "excited"]
-input = "happy"
 
 genres = spotify.recommendation_genre_seeds()
 
@@ -56,11 +54,22 @@ data_dict = {
   "danceability": []
 }
   
-  
+# print (genres)
+# print (genres.index('pop'))
+# print (genres.index('hip-hop'))
+# print (genres.index('chill'))
+# print (genres.index('indie-pop'))
+# print (genres.index('indie'))
+# print (genres.index('rock'))
+# print (genres.index('sad'))
+
+# choose only genres for pop, hiphop, chill, indiepop, indie, rock, and sad, respectively
+genrez = [genres[86], genres[52], genres[16], genres[58], genres[59], genres[99], genres[103]]
+
 # Get recommendation for each genre
-for gender in tqdm(genres[:10]):
+for gender in tqdm(genrez):
       
-  recs = spotify.recommendations(genres = [gender], limit = 3)
+  recs = spotify.recommendations(genres = [gender], limit = 7)
   recs = eval(recs.json().replace("null", "-999").replace("false", "False").replace("true", "True"))["tracks"]
   
   for track in recs:
@@ -89,48 +98,65 @@ moods = [25, 25, 25, 25, 25]
 moodvals = np.array(moods)
 
 # valence = 1.0 correlates with happiness and positve emotions, 0.0 correlates with negative emotions
+# start with neutral score of 0.5
 # happiness and sadness have most impact, other emotions have half as much impact
-valenceval = moodvals[0] - moodvals[1] - (0.5*(moodvals[2])) + (0.5*(moodvals[3])) - (0.5*(moodvals[4])) 
+valenceval = 0.5
+# if happiness is 50, can get max of 1.0
+valenceval = valenceval + (moodvals[0] * 0.01)  - (moodvals[1] * 0.01) - (moodvals[2] * 0.005) + (moodvals[3] * 0.005) - (moodvals[4] * 0.005)
 # max val is 50 happiness, 50 romantic, so divide by 75 as max value
-valenceval = valenceval / (float)(75) 
-# if more negative emotions, set as 0
 if valenceval < 0:
   valenceval = 0.0
 
 # energy = 1.0 correlates with intensity, like death metal, 0.0 correlates with mellowness
 # here, we base energy value on happiness and anger only
-energyval = (moodvals[0] + moodvals[2]) / (float)(100);
+# start with base value 0.5
+# if happiness and anger maxed, get 1.0
+energyval = 0.5
+energyval = energyval + (moodvals[0] * 0.005) + (moodvals[1] * 0.005) 
 
 # danceability = 1.0 correlates with most danceable, 0.0 correlates with least danceable
+# start w/ neutral val 0.5, subtract based on sadness, anxious, anger, romantic
 # here, we base danceability on happiness, but want less danceable songs based on if you are sad, angry, or anxious
-danceabilityval = moodvals[0] - moodvals[1] - (0.2*(moodvals[2])) - (0.2*(moodvals[4]));
-danceabilityval = danceabilityval / 50;
+danceabilityval = 0.5
+danceabilityval = danceabilityval - (moodvals[1] * 0.0025) - (moodvals[2] * 0.005) - (moodvals[4] * 0.0025)
+if danceabilityval < 0:
+    danceabilityval = 0.0
 
 
 print (df)
 def getSong(input, df):
     foundSong = False
+    # print(danceabilityval)
+    # print(valenceval)
     for i in range(10):
         optimalValence = (df.loc[i].at["valence"] > valenceval - 0.1 and df.loc[i].at["valence"] <= valenceval + 0.1)
         optimalEnergy = (df.loc[i].at["energy"] > energyval - 0.1 and df.loc[i].at["energy"] <= energyval + 0.1)
         optimalDance = (df.loc[i].at["danceability"] > danceabilityval - 0.1 and df.loc[i].at["danceability"] <= danceabilityval + 0.1)
         if (optimalValence and optimalEnergy and optimalDance):
-            foundSong = true
-            print(df.loc[i].at["artist_name"])
-            print(df.loc[i].at["track_name"])
+            foundSong = True
+            print(df.loc[i].at["artist_name"], end = "  - ")
+            print(df.loc[i].at["track_name"], end = " ")
+            print ("https://open.spotify.com/track/", end="")
+            print (df.loc[i].at["id"])
+            #print(df.loc[i].at["id"])
             break
-    if not foundSong: #again with larger margins
+    if not foundSong: 
+      # try again with larger margins
         for i in range(10):
-            optimalValence = (df.loc[i].at["valence"] > valenceval - 0.2 and df.loc[i].at["valence"] <= valenceval + 0.2)
-            optimalEnergy = (df.loc[i].at["energy"] > energyval - 0.2 and df.loc[i].at["energy"] <= energyval + 0.2)
-            optimalDance = (df.loc[i].at["danceability"] > danceabilityval - 0.5 and df.loc[i].at["danceability"] <= danceabilityval + 0.5)
+            optimalValence = (df.loc[i].at["valence"] > valenceval - 0.3 and df.loc[i].at["valence"] <= valenceval + 0.3)
+            optimalEnergy = (df.loc[i].at["energy"] > energyval - 0.3 and df.loc[i].at["energy"] <= energyval + 0.3)
+            optimalDance = (df.loc[i].at["danceability"] > danceabilityval - 0.3 and df.loc[i].at["danceability"] <= danceabilityval + 0.3)
             if (optimalValence and optimalEnergy and optimalDance):
-                foundSong = true
-                print(df.loc[i].at["artist_name"])
-                print(df.loc[i].at["track_name"])
+                foundSong = True
+                print(df.loc[i].at["artist_name"], end = " - ")
+                print(df.loc[i].at["track_name"], end = " ")
+                print ("https://open.spotify.com/track/", end="")
+                print(df.loc[i].at["id"])
+                #print(df.loc[i].at["id"])
                 break
     if not foundSong:
-        print("Rick Astley")
-        print("Never Gonna Give You Up")
+        print("Rick Astley", end = " - ")
+        print("Never Gonna Give You Up", end = " ")
+        print("https://open.spotify.com/track/4cOdK2wGLETKBW3PvgPWqT")
 
 getSong(input, df)
